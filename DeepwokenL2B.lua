607dacc79136122319c3f674233966716afffe3e
PeeblYweeb <74932007+PeeblyWeeb@users.noreply.github.com>
2023-03-18 06:59:52 -0400

    Add the acutal script
    ur welcome

---
+++ DeepwokenL2B.lua
@@ -0,0 +1,23 @@
+if (game.PlaceId) == 4111023553 or game.PlaceId == 6032399813
+then
+    local already_exists = isfile("layer2bell.mp3")
+    if (already_exists == false)
+    then
+        print("downloading")
+        local Response = syn.request({
+            Url = "https://github.com/PeeblyWeeb/DeepwokenL2BMenuMusic/blob/main/layer2bell.mp3?raw=true",
+            Method = "GET"
+        })
+        
+        writefile("layer2bell.mp3", Response.Body)
+    end
+    local contours = game.Players.LocalPlayer.PlayerGui.LoadingGui.LoadingGui.Music:GetChildren()
+    local layer2bell = getsynasset("layer2bell.mp3")
+    
+    for i,v in pairs(contours)
+    do
+        v.SoundId = layer2bell
+    end
+else
+    print("Not in deepwoken!")
+end
