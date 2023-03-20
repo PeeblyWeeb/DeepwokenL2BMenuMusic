431e7a8fa9828cdb6168473f2eeb5f9cd9c38bbc
PeeblyWeeb <PebblyKat@gmail.com>
2023-03-20 10:53:14 -0400

    update script to work with new audio location

--- DeepwokenL2B.lua
+++ DeepwokenL2B.lua
@@ -1,7 +1,5 @@
--- made by peeblyweeb
 -- thank you skidnik for telling me about getsynasset()
 
--- place ids
 -- depths: 5735553160
 -- mainmenu: 4111023553
 -- etrean lum: 6032399813
@@ -9,26 +7,32 @@
 -- chime: 6832944305
 -- east lum: 6473861193
 
-DeepwokenPlaceIds = {4111023553,6032399813,6473861193,5735553160,8668476218,6832944305}
+local DeepwokenPlaceIds = {4111023553,6032399813,6473861193,5735553160,8668476218,6832944305}
+local Player = game.Players.LocalPlayer
 
 
-game.Loaded:Wait()
+-- game.Loaded:Wait()
+
+function download(url) 
+    local Data = syn.request({
+        Url = url,
+        Method = "GET"
+    }).Body
+    return Data
+end
 
 if table.find(DeepwokenPlaceIds,game.PlaceId) then
-    if (isfile("layer2bell.mp3") == false)
-    then
-        local Response = syn.request({
-            Url = "https://github.com/PeeblyWeeb/DeepwokenL2BMenuMusic/blob/main/layer2bell.mp3?raw=true",
-            Method = "GET"
-        })
-        
-        writefile("layer2bell.mp3", Response.Body)
+    if isfolder("dwl2bas") == false then
+        makefolder("dwl2bas")
     end
-    local contours = game.Players.LocalPlayer.PlayerGui.LoadingGui.LoadingGui.Music:GetChildren()
-    local layer2bell = getsynasset("layer2bell.mp3")
-    
-    for i,v in pairs(contours)
-    do
-        v.SoundId = layer2bell
+    if isfile("dwl2bas/MenuMusic.mp3") == false then
+        file = download("https://github.com/PeeblyWeeb/DeepwokenL2BMenuMusic/blob/main/audio/MenuMusic.mp3")
+        writefile("dwl2bas/MenuMusic.mp3", file)
+    end
+
+    local MenuContours = Player.PlayerGui.LoadingGui.LoadingGui.Music:GetChildren()
+    local NewMenuMusic = getsynasset("dwl2bas/MenuMusic.mp3")
+    for i,Contour in pairs(MenuContours) do
+        Contour.SoundId = NewMenuMusic
     end
 end
